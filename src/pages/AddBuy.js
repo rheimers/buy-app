5a981d6cfaf1eb8eeebd27e57d7f2c6c8b0ad1b0
rheimers <regineheimers@gmail.com>
2020-08-23 12:04:02 +0200

    Create form to add new item and qty

--- src/pages/AddBuy.js
+++ src/pages/AddBuy.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function AddBuy() {
+  const [item, setItem] = useState("");
+  const [qty, setQty] = useState("");
+
+  function handleItemChange(event) {
+    setItem(event.target.value);
+  }
+
+  function handleQtyChange(event) {
+    setQty(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert("Submitted" + item + qty);
+  }
+
   return (
-    <div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Item:
+          <input value={item} onChange={handleItemChange} />
+        </label>
+        <label>
+          QTY:
+          <input value={qty} onChange={handleQtyChange} />
+        </label>
+        <input type="submit" value="ADD BUY" />
+      </form>
       <Link to="/">BUY</Link>
-      ADD BUY
-    </div>
+    </>
   );
 }
 
